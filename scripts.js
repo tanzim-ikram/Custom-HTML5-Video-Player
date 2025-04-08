78bc69179637a84779a1f7f45bfab84a52b51a68
Tanzim Ikram Sheikh <tanzim.ikram@gmail.com>
2025-04-09 01:41:28 +0600

    Get Elements

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.toggle');
+const toggle = player.querySelector('.progress__filled');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const range = player.querySelectorAll('.player__slider');
+
+// Functions
