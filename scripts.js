fbb46cb297b8e065aa7a53472295ee2d2ae65de4
Tanzim Ikram Sheikh <tanzim.ikram@gmail.com>
2025-04-09 01:47:35 +0600

    Add togglePlay function

--- scripts.js
+++ scripts.js
@@ -8,3 +8,17 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const range = player.querySelectorAll('.player__slider');
 
 // Functions
+function togglePlay() {
+    // Shortcut method for Toggle functionality
+    // const method = video.paused ? 'play' : 'pause';
+    // video[method]();
+
+    if (video.paused) {
+        video.play();
+    }
+    else {
+        video.pause();
+    }
+}
+
+// Event Listeners
